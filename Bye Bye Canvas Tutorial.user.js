468916289d97ce1f73e69eb75f7392cc3f3ceacf
paulickj <55640316+paulickj@users.noreply.github.com>
2020-09-16 22:31:15 -0500

    Adds the js script

---
+++ Bye Bye Canvas Tutorial.user.js
@@ -0,0 +1,9 @@
+/*
+==UserScript==
+@name             Bye Bye Canvas Tutorial
+@match            *.instructure.com/*
+@author           paulickj
+@version          1.0
+==/UserScript==
+*/
+function GM.setValue("canvas-tourpoints-shown-student", "true")
